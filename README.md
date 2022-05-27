843f1eb80be1a32a789302d81d2ed8aad71abdf1
jupiter <brianbrainrogers@gmail.com>
2022-05-27 22:12:24 +0100

    Slight change to style and README

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # Mock form
 
-From the [Odin Project](https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/forms).
+A form with live frontend validation using vanilla JavaScript. Assignment for the Odin Project.
 
-Build a browser form which collects Email, Country, Zip Code, Password and Password Confirmation fields.
+[Live Page](https://tomahawk-jupiter.github.io/mock-form/)
 
-## Should Include
+When a user leaves a field it is automatically validated. It is then highlighted until filled in correctly.
 
-1. **Live inline validation:** Highlight and display message until field filled in correctly. When a user leaves a field it should be automatically validated.
+There is an error or success message when the form is submitted. The form isn't connected to anything.
 
-2. **Error message with invalid form submission:** If any active errors or missing required fields.
+## Overview
 
-## Notes
+### JavaScript
 
-Turn off browser auto validate:
+Each form input has a listener listening for the blur event. When its triggered, it checks if the input is valid with the `element.validity.valid` method.
+
+If its invalid an error class is added to the element and a message is displayed. Another listener for the event `input` is added to the element that watches for the input to meet the requirements.
+
+When the value becomes valid the valid class is added. The classes are used to add the different styles.
+
+### HTML
+
+RegEx can be used to validate input element values using the `pattern` attribute.
+
+## Useful Notes
+
+### Turn off browser auto validate
+
+Set this attribute on the form to disable the default validation behaviour so that you can set your own.
 
     <form novalidate>
+
+### Blur Event
+
+Listen for the `blur` event in the event listeners. This event happens when an element loses focus.
+
+### Check an elements validity
+
+    element.validity.valid
--- index.js
+++ index.js
@@ -1,136 +1,143 @@
-const form = document.querySelector('form');
-const email = document.querySelector('#email');
-const emailError = document.querySelector('#email-error');
-const country = document.querySelector('#country');
-const countryError = document.querySelector('#country-error');
-const postcode = document.querySelector('#postcode');
-const postcodeError = document.querySelector('#postcode-error');
-const password = document.querySelector('#password');
-const passwordError = document.querySelector('#password-error');
-const confirmPassword = document.querySelector('#confirm-password');
-const confirmPasswordError = document.querySelector('#confirm-password-error');
+const form = document.querySelector("form");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email-error");
+const country = document.querySelector("#country");
+const countryError = document.querySelector("#country-error");
+const postcode = document.querySelector("#postcode");
+const postcodeError = document.querySelector("#postcode-error");
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#password-error");
+const confirmPassword = document.querySelector("#confirm-password");
+const confirmPasswordError = document.querySelector("#confirm-password-error");
 let password1;
 let formIsValid = false;
 
-email.addEventListener('blur', (e) => {
+email.addEventListener("blur", (e) => {
   if (email.validity.valid) {
-    email.setAttribute('class', 'valid');
-    emailError.innerText = '';
+    email.setAttribute("class", "valid");
+    emailError.innerText = "";
     formIsValid = true;
   } else {
     formIsValid = false;
-    email.setAttribute('class', 'error');
-    emailError.innerText = 'Please enter a valid email!'
-    email.addEventListener('input', () => {
+    email.setAttribute("class", "error");
+    emailError.innerText = "Please enter a valid email!";
+
+    email.addEventListener("input", () => {
       if (email.validity.valid) {
-        email.setAttribute('class', 'valid');
-        emailError.innerText = '';
+        email.setAttribute("class", "valid");
+        emailError.innerText = "";
       } else {
-        email.setAttribute('class', 'error');
-        emailError.innerText = 'Please enter a valid email!'
+        email.setAttribute("class", "error");
+        emailError.innerText = "Please enter a valid email!";
       }
-    })
+    });
   }
 });
 
-country.addEventListener('blur', (e) => {
+country.addEventListener("blur", (e) => {
   if (country.validity.valid) {
     formIsValid = true;
-    country.setAttribute('class', 'valid');
-    countryError.innerText = '';
+    country.setAttribute("class", "valid");
+    countryError.innerText = "";
   } else {
     formIsValid = false;
-    country.setAttribute('class', 'error');
-    countryError.innerText = 'Required field, only letter characters!'
-    country.addEventListener('input', () => {
+    country.setAttribute("class", "error");
+    countryError.innerText = "Required field, only letter characters!";
+
+    country.addEventListener("input", () => {
       if (country.validity.valid) {
-        country.setAttribute('class', 'valid');
-        countryError.innerText = '';
+        country.setAttribute("class", "valid");
+        countryError.innerText = "";
       } else {
-        country.setAttribute('class', 'error');
-        countryError.innerText = 'Required field, only letter characters!'
+        country.setAttribute("class", "error");
+        countryError.innerText = "Required field, only letter characters!";
       }
-    })
+    });
   }
 });
 
-postcode.addEventListener('blur', (e) => {
+postcode.addEventListener("blur", (e) => {
   if (postcode.validity.valid) {
     formIsValid = true;
-    postcode.setAttribute('class', 'valid');
-    postcodeError.innerText = '';
+    postcode.setAttribute("class", "valid");
+    postcodeError.innerText = "";
   } else {
     formIsValid = false;
-    postcode.setAttribute('class', 'error');
-    postcodeError.innerText = 'Doesn\'t look quite right!'
-    postcode.addEventListener('input', () => {
+    postcode.setAttribute("class", "error");
+    postcodeError.innerText = "Doesn't look quite right!";
+
+    postcode.addEventListener("input", () => {
       if (postcode.validity.valid) {
-        postcode.setAttribute('class', 'valid');
-        postcodeError.innerText = '';
+        postcode.setAttribute("class", "valid");
+        postcodeError.innerText = "";
       } else {
-        postcode.setAttribute('class', 'error');
-        postcodeError.innerText = 'Doesn\'t look quite right!'
+        postcode.setAttribute("class", "error");
+        postcodeError.innerText = "Doesn't look quite right!";
       }
-    })
+    });
   }
 });
 
-password.addEventListener('blur', (e) => {
+password.addEventListener("blur", (e) => {
   if (password.validity.valid) {
     formIsValid = true;
-    password.setAttribute('class', 'valid');
-    passwordError.innerText = '';
+    password.setAttribute("class", "valid");
+    passwordError.innerText = "";
     password1 = e.target.value;
   } else {
     formIsValid = false;
-    password.setAttribute('class', 'error');
-    passwordError.innerText = 'Minimum 8 characters!'
-    password.addEventListener('input', () => {
+    password.setAttribute("class", "error");
+    passwordError.innerText = "Minimum 8 characters!";
+
+    password.addEventListener("input", () => {
       if (password.validity.valid) {
-        password.setAttribute('class', 'valid');
-        passwordError.innerText = '';
+        password.setAttribute("class", "valid");
+        passwordError.innerText = "";
       } else {
-        password.setAttribute('class', 'error');
-        passwordError.innerText = 'Minimum 8 characters!'
+        password.setAttribute("class", "error");
+        passwordError.innerText = "Minimum 8 characters!";
       }
-    })
+    });
   }
 });
 
-confirmPassword.addEventListener('blur', (e) => {
+confirmPassword.addEventListener("blur", (e) => {
   const password2 = e.target.value;
   if (confirmPassword.validity.valid && password1 === password2) {
     formIsValid = true;
-    confirmPassword.setAttribute('class', 'valid');
-    confirmPasswordError.innerText = '';
+    confirmPassword.setAttribute("class", "valid");
+    confirmPasswordError.innerText = "";
   } else {
     formIsValid = false;
-    confirmPassword.setAttribute('class', 'error');
-    confirmPasswordError.innerText = 'Incorrect password!'
-    confirmPassword.addEventListener('input', () => {
+    confirmPassword.setAttribute("class", "error");
+    confirmPasswordError.innerText = "Incorrect password!";
+
+    confirmPassword.addEventListener("input", () => {
       const password2 = e.target.value;
       if (confirmPassword.validity.valid && password1 === password2) {
-        confirmPassword.setAttribute('class', 'valid');
-        confirmPasswordError.innerText = '';
+        confirmPassword.setAttribute("class", "valid");
+        confirmPasswordError.innerText = "";
       } else {
-        confirmPassword.setAttribute('class', 'error');
-        confirmPasswordError.innerText = 'Incorrect password!'
+        confirmPassword.setAttribute("class", "error");
+        confirmPasswordError.innerText = "Incorrect password!";
       }
-    })
+    });
   }
 });
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (formIsValid && email.validity.valid
-    && country.validity.valid
-    && postcode.validity.valid
-    && password.validity.valid
-    && confirmPassword.validity.valid) {
-    confirmPasswordError.innerText = 'Everything looks good!';
-    confirmPasswordError.style.color = 'green';
+  if (
+    formIsValid &&
+    email.validity.valid &&
+    country.validity.valid &&
+    postcode.validity.valid &&
+    password.validity.valid &&
+    confirmPassword.validity.valid
+  ) {
+    confirmPasswordError.innerText = "Everything looks good!";
+    confirmPasswordError.style.color = "green";
   } else {
-    confirmPasswordError.innerText = 'Something doesn\'t look right!'
+    confirmPasswordError.innerText = "Something doesn't look right!";
   }
-
 });
